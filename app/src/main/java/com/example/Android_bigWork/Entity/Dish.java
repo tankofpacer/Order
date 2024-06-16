53c196ee2e82539bc5af26d3a5d8b7478941b1a4
tankofpacer <59638717+tankofpacer@users.noreply.github.com>
2024-06-17 02:15:57 +0800

    更新购物车弹窗
    更新购物车弹窗

--- app/src/main/java/com/example/Android_bigWork/Entity/Dish.java
+++ app/src/main/java/com/example/Android_bigWork/Entity/Dish.java
@@ -1,8 +1,5 @@
 package com.example.Android_bigWork.Entity;
 
-import androidx.room.Entity;
-
-@Entity(tableName = "dish_table")//实体类的声明
 public class Dish {
     private int GID;
     private String name;
@@ -12,12 +9,12 @@ public class Dish {
     private int CID;
     private boolean customizable;
 
-    public Dish(String name, double price) {
-        new Dish(0, name, "no description", price, "popular", 0, false);
+    public Dish(String name,double price) {
+        new Dish(0,name,"no description",price,"popular",0,false);
     }
 
     public Dish(int gid, String name, String description, double price, String category, int CID, boolean customizable) {
-        this.GID = gid;
+        this.GID=gid;
         setName(name);
         setDescription(description);
         setPrice(price);
